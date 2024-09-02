a89e35a0c1b884d9adc7c9b98c97077bdf4cb662
Eduardo Thomas <eduardothomas64@gmail.com>
2024-09-02 03:00:41 -0500

    Added basic gameplay

---
+++ scenes/level.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_timer_timeout() -> void:
+	print("meteor")
+
+func _ready() -> void:
+	print("It is working")
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed: int = 500
 
@@ -10,5 +10,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
-	
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
